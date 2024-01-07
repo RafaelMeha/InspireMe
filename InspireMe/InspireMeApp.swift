b96d6b0b64bfeaca681a2abec3ab1f43f92d31d2
Rafael Meha <rafaelmeha5@gmail.com>
2024-01-07 00:17:50 +0000

    Initial Commit

---
+++ InspireMe/InspireMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspireMeApp.swift
+//  InspireMe
+//
+//  Created by Rafael Meha on 07/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct InspireMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
